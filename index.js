0d04052d58ee6b820263d9d34d4ffd9be07b7d67
daebrayk <daebrayk@proton.me>
2026-06-15 14:54:38 +0800

    You can ping heifer now and get a latency report

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // stuffs for importing libs :D
 
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, REST, Routes, SlashCommandBuilder } = require('discord.js');
 
 // client instance... obviously.
 
@@ -14,6 +14,30 @@ const client = new Client({
   ]
 });
 
+// Slash command builder and stuffs, so Heifer is more useful!!
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription("Heifer isn't alive silly, but this will see if he works!")
+].map(cmd => cmd.toJSON());
+
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands }
+    );
+    console.log('Slash commands registered.');
+  } catch (err) {
+    console.error(err);
+  }
+})();
+
+
 // log into console when the bot isnt too shy hehe
 
 client.once('ready', () => {
@@ -25,6 +49,16 @@ client.once('ready', () => {
 client.on('guildMemberAdd', (member) => {
     const channel = member.guild.systemChannel;
   channel.send(`More friends! Hi ${member.user}!`);
+});
+
+//play ping pong with heifer!! (checks latency lol) 
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply(`🏓 Pong! Latency: **${client.ws.ping}ms**`);
+  }
 });
 
 
